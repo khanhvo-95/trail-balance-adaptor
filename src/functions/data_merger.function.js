71ab95eb1943392bd935a3701498a17c2ca98b33
EPAM\Khanh_Vo <khanh_vo@epam.com>
2023-12-06 15:10:34 +0700

    update data merger function

--- src/functions/data_merger.function.js
+++ src/functions/data_merger.function.js
@@ -1,16 +1,17 @@
 import { app } from '@azure/functions';
 import { format, toDate, isValid } from "date-fns";
+import TrialBalanceDAO from "../db/trial_balance_dao.js";
 
 function generateAdaptorSpecificProperties(tbData) {
-  tbData.JournalEntryCreationDate = format(new Date(), DATE_FORMAT);
-  tbData.InterfaceGroupIdentifier = format(new Date(), DATE_FORMAT_WITH_HOUR);
+  tbData.JournalEntryCreationDate = format(new Date(), 'yyyy/MM/dd');
+  tbData.InterfaceGroupIdentifier = format(new Date(), 'ddMMyyyyHHmm');
   let channelCode = "CHANNEL_CODE_FOR_MFI";
   const attribute1 =
-    tbData.Segment5 +
+    tbData.segment4 +
     channelCode +
-    tbData.Segment4 +
-    tbData.Segment3 +
-    tbData.Segment2;
+    tbData.segment4 +
+    tbData.segment3 +
+    tbData.segment2;
   tbData.Attribute1 = attribute1;
   tbData.ReconciliationReference = attribute1;
 }
@@ -18,31 +19,52 @@ function generateAdaptorSpecificProperties(tbData) {
 function populateAccountingDate(tbData) {
   const date = toDate(tbData.EffectiveDateOfTransaction);
   if (isValid(date)) {
-    tbData.AccountingDate = format(date, DATE_FORMAT);
+    tbData.AccountingDate = format(date, 'yyyy/MM/dd');
   } else {
     // Handle the invalid date case, e.g., set a default value or throw an error
     tbData.AccountingDate = null;
   }
 }
 
-export async function mergeData(request, context) {
-    context.log(`Http function processed request for url "${request.url}"`);
+async function retrieveTrialBalanceData(startId) {
+  const trialBalanceDao = new TrialBalanceDAO();
+  const responseData = await trialBalanceDao.getTrialBalanceDetails(startId);
+  if (!responseData.length) {
+    return [];
+  }
+  return responseData;
+}
 
-    const tbDataList = tbDataFromODS.map((dataFromDB) => {
-        const tbData = { ...dataFromDB, ...staticData };
-    
-        populateAccountingDate(tbData);
-        generateAdaptorSpecificProperties(tbData);
-    
-        return tbData;
-      });
-    
-      return tbDataList;
+function mergeData(tbDataFromODS, staticData) {
+  const staticDataObj = Object.fromEntries(staticData);
+
+  const tbDataList = tbDataFromODS.map((dataFromDB) => {
+    const tbData = { ...dataFromDB, ...staticDataObj };
+
+    populateAccountingDate(tbData);
+    generateAdaptorSpecificProperties(tbData);
+
+    return tbData;
+  });
+
+  return tbDataList;
 }
 
 app.http('data_merger', {
-    methods: ['POST'],
+    methods: ['GET'],
     authLevel: 'anonymous',
     route: 'data-processors',
-    handler: async (request, context) => mergeData(request, context)
+    handler: async (request, context) => { 
+      context.log(`Http function processed request for url "${request.url}"`);
+
+      const tbDataFromODS = await retrieveTrialBalanceData(1);
+      const staticData = new Map([
+        ["key1", "value1"],
+        ["key2", "value2"],
+        ["key3", "value3"],
+      ]);
+    
+      const tbDataList = mergeData(tbDataFromODS, staticData);
+      context.res = { body: tbDataList };
+    }
 });
